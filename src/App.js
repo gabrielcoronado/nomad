f9369525b72b3e62436e5875e27d296b279f4b8d
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-17 07:44:10 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPerson } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa";
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav((prev) => !prev);
+  };
+
   return (
-    <div className="flex justify-between items-center h-20 px-4">
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white">
       <div>
-        <h1>NOMAD.</h1>
+        <h1 className={nav ? "hidden" : ""}>NOMAD.</h1>
       </div>
-      <ul className="flex">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>Destination</li>
         <li>Travel</li>
         <li>View</li>
         <li>Book</li>
       </ul>
-      <div className="flex">
-        <BiSearch />
-        <BsPerson />
+
+      {/* Burguer Menu Icon */}
+      <div className="hidden md:flex">
+        <BiSearch className="mr-2" size={20} />
+        <BsPerson size={20} />
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      <div onClick={handleNav} className="md:hidden z-10">
+        {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20} />}
+      </div>
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-[1.4rem] flex flex-col"
+            : "absolute left-[-100%]"
+        }
+      >
+        <ul>
+          <h1>NOMAD.</h1>
+          <li className="border-b">Home</li>
+          <li className="border-b">Destination</li>
+          <li className="border-b">Travel</li>
+          <li className="border-b">View</li>
+          <li className="border-b">Book</li>
+          <div className="flex flex-col">
+            <button className="my-6">Search</button>
+            <button>Account</button>
+          </div>
+          <div className="flex justify-between my-6">
+            <FaFacebook className="icon" />
+            <FaTwitter className="icon" />
+            <FaYoutube className="icon" />
+            <FaPinterest className="icon" />
+            <FaInstagram className="icon" />
+          </div>
+        </ul>
       </div>
     </div>
   );
