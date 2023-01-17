48cfb3f5075bddb5c01dd832b88a3c3dbf51b367
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-17 14:35:34 -0500

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Destinations from "./components/Destinations";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import Selects from "./components/Selects";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <Destinations />
+      <Search />
+      <Selects />
     </div>
   );
 };
