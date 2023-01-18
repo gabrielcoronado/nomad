5bde378a370f7cbf1db6ea6be4d32334e4042921
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-18 09:34:22 -0500

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import Carousel from "./components/Carousel";
 import Destinations from "./components/Destinations";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
@@ -12,6 +14,8 @@ const App = () => {
       <Destinations />
       <Search />
       <Selects />
+      <Carousel />
+      <Footer />
     </div>
   );
 };
--- src/components/Selects.jsx
+++ src/components/Selects.jsx
@@ -9,7 +9,7 @@ import SelectsCard from "./SelectsCard";
 
 const Selects = () => {
   return (
-    <div className="max-w-[1240px] mx-ato px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <SelectsCard bg={BoraBora} text="Bora Bora" />
       <SelectsCard bg={BoraBora2} text="Bora Bora" />
       <SelectsCard bg={Maldives} text="Maldives" />
